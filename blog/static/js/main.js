a711a6b07934153c53b4f42a47067eb6ef9c374a
Denis <denka0087@gmail.com>
2018-02-14 00:00:01 +0300

    Small main.js refactoring

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -1,13 +1,11 @@
 jQuery(document).ready(function () {
 
-    if (is_touch_device())
-    {
+    if (is_touch_device()) {
         jQuery('.full-screen-scroll article').css('opacity', '1', '!important');
     }
 
     var ua = navigator.userAgent.toLowerCase();
-    if ((ua.indexOf("safari/") !== -1 && ua.indexOf("windows") !== -1 && ua.indexOf("chrom") === -1) || is_touch_device())
-    {
+    if ((ua.indexOf("safari/") !== -1 && ua.indexOf("windows") !== -1 && ua.indexOf("chrom") === -1) || is_touch_device()) {
 
         jQuery("html").css('overflow', 'auto');
 
@@ -16,9 +14,15 @@ jQuery(document).ready(function () {
             return false;
         });
 
-    } else
-    {
-        jQuery("html, .menu-left-part, #cbp-bislideshow.scroll").niceScroll({cursorcolor: "#5B5B5B", scrollspeed: 100, mousescrollstep: 80, cursorwidth: "12px", cursorborder: "none", cursorborderradius: "0px"});
+    } else {
+        jQuery("html, .menu-left-part, #cbp-bislideshow.scroll").niceScroll({
+            cursorcolor: "#5B5B5B",
+            scrollspeed: 100,
+            mousescrollstep: 80,
+            cursorwidth: "12px",
+            cursorborder: "none",
+            cursorborderradius: "0px"
+        });
 
         //Scroll Top animation
         jQuery(".scroll-top").click(function () {
@@ -32,11 +36,12 @@ jQuery(document).ready(function () {
 
 
     //Placeholder show/hide
-    jQuery('input, textarea').focus(function () {
+    var textareaInput = jQuery('input, textarea');
+    textareaInput.focus(function () {
         jQuery(this).data('placeholder', jQuery(this).attr('placeholder'));
         jQuery(this).attr('placeholder', '');
     });
-    jQuery('input, textarea').blur(function () {
+    textareaInput.blur(function () {
         jQuery(this).attr('placeholder', jQuery(this).data('placeholder'));
     });
 
@@ -58,15 +63,14 @@ jQuery(document).ready(function () {
 });
 
 
-
 jQuery(window).load(function () {
 
     jQuery(".blog-item-holder").hover(function () {
-        jQuery(".blog-item-holder").not(this).addClass('blur');
-    },
-            function () {
-                jQuery(".blog-item-holder").removeClass('blur');
-            });
+            jQuery(".blog-item-holder").not(this).addClass('blur');
+        },
+        function () {
+            jQuery(".blog-item-holder").removeClass('blur');
+        });
 
 
 //Set menu
@@ -80,43 +84,22 @@ jQuery(window).load(function () {
 //Set each image slider
     jQuery(".image-slider").each(function () {
         var id = jQuery(this).attr('id');
-        if (window[id + '_pagination'] == 'true')
-        {
-            var pagination_value = '.' + id + '_pagination';
-        } else
-        {
-            var pagination_value = false;
-        }
-
-        var auto_value = window[id + '_auto'];
-        if (auto_value == 'false')
-        {
-            auto_value = false;
-        } else {
-            auto_value = true;
-        }
-
-        var hover_pause = window[id + '_hover'];
-        if (hover_pause == 'true')
-        {
-            hover_pause = 'resume';
-        } else {
-            hover_pause = false;
-        }
-
-        var speed_value = window[id + '_speed'];
+        var paginationValue = window[id + '_pagination'] === 'true';
+        var autoValue = window[id + '_auto'] === 'true';
+        var hoverPause = window[id + '_hover'] === 'true';
+        var speedValue = window[id + '_speed'];
 
         jQuery('#' + id).carouFredSel({
             responsive: true,
             width: 'variable',
             auto: {
-                play: auto_value,
-                pauseOnHover: hover_pause
+                play: autoValue,
+                pauseOnHover: hoverPause
             },
-            pagination: pagination_value,
+            pagination: paginationValue,
             scroll: {
                 fx: 'crossfade',
-                duration: parseFloat(speed_value)
+                duration: parseFloat(speedValue)
             },
             swipe: {
                 onMouse: true,
@@ -137,10 +120,6 @@ jQuery(window).load(function () {
 
 //Show-Hide header sidebar
     jQuery('#toggle').on("click", multiClickFunctionStop);
-    
-    //Fix for sidebar height
-    jQuery("#sidebar").css('minHeight', jQuery("#content").outerHeight());
-
 });
 
 
@@ -183,12 +162,13 @@ function is_touch_device() {
 jQuery(window).bind("scroll", function () {
     if (jQuery(this).scrollTop() > 700) {
         jQuery('.scroll-top').fadeIn(500);
-    } else
-    {
+    } else {
         jQuery('.scroll-top').fadeOut(500);
     }
 });
 
+
+// todo: fucking get rid of this!!!
 function isValidEmailAddress(emailAddress) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(emailAddress);
@@ -214,8 +194,7 @@ var SendMail = function () {
             success: function (response) {
                 if (response) {
                     var responseObj = jQuery.parseJSON(response);
-                    if (responseObj.ResponseData)
-                    {
+                    if (responseObj.ResponseData) {
                         alert(responseObj.ResponseData);
                     }
                 }
@@ -223,8 +202,7 @@ var SendMail = function () {
             error: function (xhr, ajaxOptions, thrownError) {
                 //xhr.status : 404, 303, 501...
                 var error = null;
-                switch (xhr.status)
-                {
+                switch (xhr.status) {
                     case "301":
                         error = "Redirection Error!";
                         break;
@@ -248,8 +226,7 @@ var SendMail = function () {
                 }
             }
         });
-    } else
-    {
+    } else {
         alert('Your email is not in valid format');
     }
 };
